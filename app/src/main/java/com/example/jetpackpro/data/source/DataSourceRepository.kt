1a02f6ddac00fef063ea552a57e3582dcf0a172d
Muhamad Irvan Dandung <irvandandung1@gmail.com>
2020-12-12 08:39:00 +0700

    inject datasource factory paging in funct getAllListMovie & getAllListTvShow

--- app/src/main/java/com/example/jetpackpro/data/source/DataSourceRepository.kt
+++ app/src/main/java/com/example/jetpackpro/data/source/DataSourceRepository.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackpro.data.source
 
 import androidx.lifecycle.LiveData
+import androidx.paging.PagedList
 import com.example.jetpackpro.data.movieentity.Movie
 import com.example.jetpackpro.data.movieentity.MovieFavorite
 import com.example.jetpackpro.data.movieentity.Result
@@ -10,7 +11,7 @@ import com.example.jetpackpro.vo.Resource
 
 interface DataSourceRepository {
     //movie
-    fun getAllMovies() : LiveData<Resource<List<Result>?>>
+    fun getAllMovies() : LiveData<Resource<PagedList<Result>?>>
     fun getDetailMovies(movieId : Int) : LiveData<Resource<Movie?>>
     fun getAllFavoriteMovies() : LiveData<List<MovieFavorite>>
     fun getCountMovieById(id : Int) : LiveData<Int>
@@ -18,7 +19,7 @@ interface DataSourceRepository {
     fun deleteFavoriteMovie(movie: MovieFavorite)
 
     //tvshow
-    fun getAllTvshow() : LiveData<Resource<List<com.example.jetpackpro.data.tvshowentity.Result>?>>
+    fun getAllTvshow() : LiveData<Resource<PagedList<com.example.jetpackpro.data.tvshowentity.Result>?>>
     fun getDetailTvShow(tvshowid: Int) : LiveData<Resource<TvShow?>>
     fun getAllFavoriteTvShow() : LiveData<List<TvShowFavorite>>
     fun getCountTvShowById(id : Int) : LiveData<Int>
